e162465a5fe907c058c2ece633d7852898c6ccfd
ilabstaff <ilabstaff@172.16.116.71>
2019-06-12 19:20:29 -0700

    thanos

--- src/forloopgauntlet.java
+++ src/forloopgauntlet.java
@@ -21,6 +21,42 @@ public static void main(String[] args) {
 		else {
 			System.out.println(i + " odd");
 		}
+		
+	}
+	for (int j = 0; j < 778; j+=7) {
+		System.out.println(j);
+	}
+	for (int i = 2010; i < 2020; i++) {
+		System.out.println("In " + i + " I was "+ (i - 2010 ));
+	}
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			System.out.println(i +" "+j);
+		}
+	}
+	for (int i = 1; i < 8; i+=3) {
+		System.out.println("");
+	
+		for (int j = 0; j < 3; j+=1) {
+			System.out.print( j + i + " ");
+		}
+	}
+	for (int i = 1; i < 92; i+=10) {
+		System.out.println("");
+		for (int j = 0; j < 10; j++) {
+			System.out.print( j + i + " ");
+		}
+	}                                       											
+	for (int i = 1; i < 7; i++) {
+		System.out.println("");
+		for (int j = 0; j <i; j++) {
+			System.out.print("* ");
+		}
+		if (i == 6)
+			System.out.println("");
+	}
+	for (int i = 0; i < 101; i++) {
+		System.out.println(100 - i);
 	}
 }
 }
