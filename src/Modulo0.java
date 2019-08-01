699a1946f35749d3d40f68fbe303f45464653dc9
ilabstaff <ilabstaff@172.16.116.77>
2019-07-31 19:30:08 -0700

    modulos

---
+++ src/Modulo0.java
@@ -0,0 +1,68 @@
+import java.awt.Color;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Modulo0 {
+public static void main(String[] args) {
+	
+	//Robot rob = new Robot();
+//rob.penDown();
+//rob.move(90);
+//rob.turn(90);
+//rob.move(90);
+//rob.turn(90);
+//rob.move(90);
+//rob.turn(90);
+//rob.move(90);
+//rob.turn(90);
+
+//JOptionPane.showMessageDialog(null, "Hello");
+
+ //String grinch = JOptionPane.showInputDialog("I will get you");
+ 
+ //String sushi = JOptionPane.showInputDialog("I WILL GET YOU!!!!!!!!!!!!");
+ //String you = JOptionPane.showInputDialog("I FOUND YOU!!!!!!!!!!!!!!!!!");
+ //String me = sushi + " " + you;
+ //JOptionPane.showMessageDialog(null, me);
+	
+	//modulo1
+	
+	//String first = JOptionPane.showInputDialog("Tell me a number");
+	//int num = Integer.parseInt(first);
+	//if (num >= 50) {
+		//JOptionPane.showMessageDialog(null, "that's a big number");
+	//}else {
+			//JOptionPane.showMessageDialog(null, "that's a little number");
+	//}
+	
+		//for (int i = 0; i < 11; i++) {
+			//System.out.println(i);
+		//}
+		//for (int i = 11; i > 0; i--) {
+			//System.out.println(i);
+		//}
+	
+	//modulo2
+	
+	
+	
+	cool();
+}
+public static void cool() {
+	//Random rnd = new Random();
+		//Robot me = new Robot();
+		//me.penDown();
+		//me.setPenColor(new Color(rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255)));
+		//me.move(90);
+		//me.turn(90);
+		//me.move(90);
+		//me.turn(90);
+		//me.move(90);
+		//me.turn(90);
+		//me.move(90);
+		//me.turn(90);
+}
+}
